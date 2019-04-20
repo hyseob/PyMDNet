5ee9d6ef80f40552b1d5dbb14e46587c6bed0aa8
hsnam <hsnam@lunit.io>
2019-04-20 17:13:45 +0000

    refactoring

--- pretrain/train_mdnet.py
+++ pretrain/train_mdnet.py
@@ -88,5 +88,5 @@ if __name__ == "__main__":
     parser.add_argument('-d', '--dataset', default='imagenet', help='training dataset {vot, imagenet}')
     args = parser.parse_args()
 
-    opts = yaml.safe_load(open('pretrain/options_{}.yaml'.format(args.dataset),'r'))
+    opts = yaml.safe_load(open('pretrain/options_{}.yaml'.format(args.dataset), 'r'))
     train_mdnet(opts)
